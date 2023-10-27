d2328e7d3715a808eaef345143625e40fa15e61b
Morris1337 <117284517+Morris1337@users.noreply.github.com>
2023-10-27 08:24:44 +0300

    `1

--- aznews/assets/js/toogle.js
+++ aznews/assets/js/toogle.js
@@ -1,14 +1,23 @@
-function toggle(button, element){
-    button.addEventListener("click", ()=>{
-        element.classList.toggle("open")
-    })
+function toggle(getButton, getElement) {
+    const button = document.querySelector(getButton);
+    const element = document.querySelector(getElement);
+
+    if (button && element) {
+        button.addEventListener("click", () => {
+            element.classList.toggle("open");
+        });
+    }
 }
 
-const menuContent = document.querySelector(".admin-menu-section")
-const iconToOpen = document.querySelector(".admin-menu-phone")
-const buttonPosition = document.querySelector(".admin-menu-icon-phone-block")
-const iconBtn = document.querySelector(".admin-menu-icon-phone-block")
+// const menuContent = document.querySelector(".admin-menu-section")
+// const iconToOpen = document.querySelector(".admin-menu-phone")
+// const buttonPosition = document.querySelector(".admin-menu-icon-phone-block")
+// const iconBtn = document.querySelector(".admin-menu-icon-phone-block")
+//competition-detals
+// const applyForAccess = document.querySelector("#apply-for-access")
+// const applicationForm = document.querySelector("#application-form")
 
-toggle(iconBtn, iconToOpen)
-toggle(iconBtn, menuContent)
-toggle(iconBtn, buttonPosition)
+toggle(".admin-menu-icon-phone-block", ".admin-menu-section")
+toggle(".admin-menu-icon-phone-block", ".admin-menu-phone")
+toggle(".admin-menu-icon-phone-block", ".admin-menu-icon-phone-block")
+toggle("#apply-for-access", "#application-form")
