9b664a143658c9b56b611f85f002f26f66b0f5de
Morris1337 <117284517+Morris1337@users.noreply.github.com>
2023-11-09 15:35:50 +0200

    lets go

--- aznews/assets/js/adminPanelTextarea.js
+++ aznews/assets/js/adminPanelTextarea.js
@@ -1,44 +1,31 @@
 
 function textarea(){
+
+    // const summernote = document.createElement("div")
+    // summernote.setAttribute("id", "summernote");
+    // form.appendChild(summernote)
+
     const textContent = document.createElement("textarea")
-    textContent.setAttribute("id", "summernote");
+    textContent.setAttribute("id", "textarea");
     textContent.classList.add("text");
     textContent.classList.add("visual");
     textContent.placeholder = "text";
     
     form.appendChild(textContent)
 
-    // br on enter
 
-    textContent.addEventListener("keydown", function (e) {
-        if (e.key === "Enter") {
-            e.preventDefault(); // Предотвращаем переход на новую строку по умолчанию
-            const startPosition = this.selectionStart;
-            const endPosition = this.selectionEnd;
-            const textBefore = this.value.substring(0, startPosition);
-            const textAfter = this.value.substring(endPosition);
-            this.value = textBefore + "<br>\n" + textAfter;
-            this.selectionStart = startPosition + 5; // Установите новую позицию курсора после <br>
-            this.selectionEnd = this.selectionStart;
-        }
-    });
     const btnSubmit = document.createElement("button")
     btnSubmit.classList.add("visual")
     btnSubmit.textContent = "submit"
     btnSubmit.addEventListener("click", ()=>{
-        
-        backend
-        
-        const newsNameInput = document.getElementById("newsName"); // Получаем элемент input по его id
-        const newsName = newsNameInput.value; // Получаем значение из input
-
-        alert("Значение из input: " + newsName); // Показываем значение в alert
+  
     })
-    add.appendChild(btnSubmit)
+    form.appendChild(btnSubmit)
+
+    $(document).ready(function() {
+        $('#textarea').summernote(); // Замените '#summernote' на селектор вашего текстового редактора
+      });
 }
-
-
-
 
 
 export {textarea}
