a5140ea1348480522698553c7c6110a1b31221dd
Morris1337 <gangstamo0ris@gmail.com>
2024-02-06 17:43:35 +0200

    update

--- aznews/assets/js/doc.js
+++ aznews/assets/js/doc.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const buttonList = document.querySelectorAll(".rules");
     const contentList = document.querySelectorAll(".content");
+    const noteikumiPdf = document.querySelectorAll(".noteikumiPdf");
+    const dopingPdf = document.querySelectorAll(".dopingsPdf");
     const liBlockList = document.querySelectorAll(".li-header-link");
 
     function blockVisible(blockIndex) {
@@ -32,29 +34,41 @@ document.addEventListener("DOMContentLoaded", function() {
     if (dataIndexParam !== null && dataIndexParam >= 0 && dataIndexParam < contentList.length) {
         blockVisible(dataIndexParam);
     }
+
+    function wakoNoteikumi(){
+        const pdfFiles = [
+            "files/noteikumi/Full_contact_noteikumi.pdf",
+            "files/noteikumi/K_1_noteikumi.pdf",
+            "files/noteikumi/Kick_light_noteikumi.pdf",
+            "files/noteikumi/WAKO_kikboksa_noteikumi.pdf",
+            "files/noteikumi/WAKO_Rules_25_10_2022.pdf"
+        ]
+        noteikumiPdf.forEach((file, index)=>{
+            file.addEventListener("click", ()=>{
+                const selectFile = pdfFiles[index];
+                window.open(selectFile, 'PDF Viewer', 'width=800,height=600');
+            });
+        })
+    }
+    wakoNoteikumi()
+
+    function antiDoping(){
+        const pdfFiles = [
+            "files/Antidoping/wada-code-2021_lv.pdf",
+            "files/Antidoping/aizliegtovielusaraksts1-1.pdf",
+            "files/Antidoping/patiesssports_vecakuunbernubrosura_73x99mm_padomi-vecakiem-un-sportistiem.pdf",
+            "files/Antidoping/patiesssports_sap_brosura_73x99mm_sportistu-un-atbalsta.per_.pdf"        
+        ]
+        dopingPdf.forEach((file, index)=>{
+            file.addEventListener("click", ()=>{
+                const selectedFile = pdfFiles[index];
+                window.open(selectedFile, 'PDF Viewer', 'width=800,height=600');
+            })
+        })
+    }
+    antiDoping()
+    
+    
 });
-
-
-function wakoNoteikumi(){
-    // openBlock()
-    filesPdf.forEach((file)=>{
-        file.addEventListener("click", ()=>{
-            const fileName = file.textContent || file.innerText;
-            if(fileName === "Full contact noteikumi"){
-                window.open("files/noteikumi/Full_contact_noteikumi.pdf", 'PDF Viewer', 'width=800,height=600');
-            }else if (fileName === "K-1 noteikumi"){
-                window.open("files/noteikumi/K_1_noteikumi.pdf", 'PDF Viewer', 'width=800,height=600');
-            }else if(fileName === "Kick light noteikumi"){
-                window.open("files/noteikumi/Kick_light_noteikumi.pdf", 'PDF Viewer', 'width=800,height=600');
-            }else if(fileName === "WAKO kikboksa noteikumi"){
-                window.open("files/noteikumi/WAKO_kikboksa_noteikumi.pdf", 'PDF Viewer', 'width=800,height=600');
-            }else{
-                window.open("files/noteikumi/WAKO_Rules_25_10_2022.pdf", 'PDF Viewer', 'width=800,height=600');
-            }
-        });
-    })
-}
-wakoNoteikumi()
-
 
 
